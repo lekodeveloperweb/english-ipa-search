483ea3b751f5b8609a25fdfa5a1d4f42f2493bb2
Alex Leko <lekodeveloperweb@hotmail.com>
2020-06-27 18:20:03 -0300

    print pronuncation in output

--- english-ipa.search.py
+++ english-ipa.search.py
@@ -7,10 +7,15 @@ service.print_option()
 prompt_input = input()
 pronunciations = service.extract_pronunciation(prompt_input)
 # print(pronunciations)
+pronunciation = ""
 for pron in pronunciations:
     if type(pron) == list:
+        pronunciation += f"{pron[1].pronounce} "
         print(
             f"WORD: {pron[1].word}\nTYPE: {pron[1].type_pronun}\n\n{pron[1].pronounce}\n")
     else:
+        pronunciation += f"{pron.pronounce} "
         print(
             f"WORD: {pron.word}\nTYPE: {pron.type_pronun}\n\n{pron.pronounce}\n")
+
+print(pronunciation)
